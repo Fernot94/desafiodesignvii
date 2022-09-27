641ccfce9449d97c341a994694674954852346fe
Fernot94 <fernoo94@hotmail.com>
2022-09-27 12:24:10 +0100

    Added Powerups

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { useState } from "react";
 import Formulario from "./Formulario";
 import Candidatos from "./Candidatos";
 import Editar from "./Editar";
+import Powerups from "./Powerups";
 
 function App() {
   const [form, setForm] = useState(false);
   const [cand, setCand] = useState(false);
+  const [power, setPower] = useState(false);
 
   return (
     <div className="App">
@@ -18,10 +20,14 @@ function App() {
         <button className="candidates" onClick={() => setCand(!cand)}>
           Candidatos
         </button>
+        <button className="powerups" onClick={() => setPower(!power)}>
+          Power Up's
+        </button>
       </div>
       <div className="body">
         {form == false ? "" : <Formulario />}
         {cand == false ? "" : <Candidatos />}
+        {power == false ? "" : <Powerups />}
       </div>
     </div>
   );
---
+++ src/Powerups.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export default function Powerups() {
+  return (
+    <div>
+      <p>Power Up 1</p>
+      <p>Power Up 2</p>
+      <p>Power Up 3</p>
+    </div>
+  );
+}
